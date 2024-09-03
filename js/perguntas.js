8c21177b827da7de8e1de77bb641eac5f4b380c7
vitorlopesbatista <129400838+vitorlopesbatista@users.noreply.github.com>
2024-09-03 08:41:32 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,105 @@
+export const perguntas = [
+    {
+        enunciado: "Os humanos deveriam parar de usar automóvel a combustão?",
+        alternativas: [
+            {
+                texto: "Sim.",
+                afirmacao: [
+                    "Automóveis a combustão produzem muito carbono, então devemos parar de usa-los.",
+                    "Automóveis devem parar de serem usados pois fazem mal ao meio-ambiente."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Não.",
+                afirmacao: [
+                    "Automóveis elétricos são caros, então temos como subistiuir tudo agora.",
+                    "Prefiro automóveis a combustão, do que automóveis elétricos."
+                ],
+                proxima: 1,
+            },
+        ]
+    },
+    {
+        enunciado: "Você acredita que o governo deveria investir mais em reciclagem?",
+        alternativas: [
+            {
+                texto: "Sim.",
+                afirmacao: [
+                    "Seria bom o governo investirar na reciclagem para ajudar o meio-ambiente.",
+                    "Se o governo investisse na recilagem teriamos um ambiente do contidiano mais limpo."
+                ],
+                proxima: 2,
+            },
+            {
+                texto: "Não.",
+                afirmacao: [
+                    "existem coisas mais importates para se investir.",
+                    "atualmente o investimento já é o suficiente."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "As pessoas devem começar a usar painés solares?",
+        alternativas: [
+            {
+                texto: "Sim.",
+                afirmacao: [
+                    "As pessoas devem usar painés solares, por mais que seja caro.",
+                    "Acredito que as pessoas devem comprar painés solares com a ajuda do governo."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Não.",
+                afirmacao: [
+                    "As pessoas não devem usar painés solares, porquê é caro.",
+                    "Acredito que podemos continuar a usar energia elétrica sem ser a de painés solares."
+                ],
+                proxima: 3,
+            },
+        ]
+    },
+    {
+        enunciado: "Devemos criar anúncios, e cartazes que conscientizem a reciclagem?",
+        alternativas: [
+            {
+                texto: "Sim.",
+                afirmacao: [
+                    "Devemos criar anúncios, e cartazes que conscientizem a reciclagem, pois as pessoas vão se conscientizer sobre a poluição.",
+                    "Devemos criar anúncios, e cartazes que conscientizem a reciclagem, porquê as pessoas vão aprender sobre os problemas ambientas relacionados à falta dela."
+                ],
+                proxima: 4,
+            },
+            {
+                texto: "Não.",
+                afirmacao: [
+                    "Não devemos criar anúncios, e cartazes que conscientizem a reciclagem, porquê as pessoas não vão se tornar conscientes apenas com a reciclagem.",
+                    "Não devemos criar anúncios, e cartazes que conscientizem a reciclagem, pois as pessoas precisam aprender a reciclar primeiro."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Deveriamos investir em energia renovavel?.",
+        alternativas: [
+            {
+                texto: "Sim.",
+                afirmacao: [
+                    "Creio que devemos investir em energia renovavel, afinal isso ajudaria o meio ambiente .",
+                    "Acho que sim, já que gerariamos menor gasto da produção de energia."
+                ],
+            },
+            {
+                texto: "Não.",
+                afirmacao: [
+                    "Não deveriamos investir em energia renovavel, já que teriamos gastos desnecessários, quando podemos continuar em outras coisas ",
+                    "Não deveriamos investir em energia renovavel, afinal não faz diferença."
+                ],
+            },
+        ]
+    }
+];
